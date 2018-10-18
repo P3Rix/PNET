318cd9cca46043b88999c8736c7bc8219fe16a29
ikkani <urrutisanchez@hotmail.com>
2018-10-18 17:40:25 +0200

    awd

--- validacion-formulario.js
+++ validacion-formulario.js
@@ -5,16 +5,23 @@ function validarFormulario(formulario){
 	var reNombre = /^[a-zA-Z]{2,25}/;
 	var reNumero = /[0-9]+/;
 	var reApellido = /[a-zA-Z]+ [a-zA-Z]+/;
+	var reLetras = /[a-zA-Z]+/;
 
 	if (!(reDNI.test(formulario.dni.value))){
 		formulario.dni.style.color = "red";
 		formulario.dni.style.fontWeight  = "bold";
 
+		document.getElementById('field_dni').style.color = "red";
+		document.getElementById('field_dni').fontWeight  = "bold";
+		
 		bool = false;
 	}
 	if ((reNumero.test(formulario.nombre.value)) || !reNombre.test(formulario.nombre.value)){
 		formulario.nombre.style.color = "red";
 		formulario.nombre.style.fontWeight  = "bold";
+
+		document.getElementById('field_nombre').style.color = "red";
+		document.getElementById('field_nombre').fontWeight  = "bold";
 
 		bool = false;
 	}
@@ -22,9 +29,43 @@ function validarFormulario(formulario){
 		formulario.apellidos.style.color = "red";
 		formulario.apellidos.style.fontWeight  = "bold";
 
+		document.getElementById('field_apellidos').style.color = "red";
+		document.getElementById('field_apellidos').fontWeight  = "bold";
+
+		bool = false;
+	}
+
+	if (reLetras.test(formulario.telefono.value)){ 
+		formulario.telefono.style.color = "red";
+		formulario.telefono.style.fontWeight  = "bold";
+
+		document.getElementById('field_telefono').style.color = "red";
+		document.getElementById('field_telefono').fontWeight  = "bold";
+
 		bool = false;
 	}
 
 
 	return bool;
 }
+
+
+function pulsoCampo(name){
+	var field = "field_"+name;
+
+
+	document.getElementById(field).style.color		  	  = "black";
+	document.getElementById(field).fontWeight			  = "normal";
+	document.getElementsByName(name)[0].style.color   	  = "black";
+	document.getElementsByName(name)[0].style.fontWeight  = "normal";
+
+}
+
+
+
+
+
+
+
+
+
