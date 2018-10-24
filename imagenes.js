ba17108d34226f5b06da708383a032a94306d5f4
peri <josemanuel.perinanfreire@alum.uca.es>
2018-10-24 13:51:36 +0200

    Localizacion random

---
+++ imagenes.js
@@ -0,0 +1,29 @@
+var imagenes=new Array(
+        'Imagenes/hotel1.jpg',
+        'Imagenes/hotel3.jpg',
+        'Imagenes/hotel4.jpg'
+    );
+
+    /**
+    * Funcion para cambiar la imagen
+    */
+    function rotarImagenes()
+    {
+        // obtenemos un numero aleatorio entre 0 y la cantidad de imagenes que hay
+        var index=Math.floor((Math.random()*imagenes.length));
+
+        // cambiamos la imagen
+        document.getElementById("imagen").src=imagenes[index];
+    }
+
+    /**
+    * Función que se ejecuta una vez cargada la página
+    */
+    onload=function()
+    {
+        // Cargamos una imagen aleatoria
+        rotarImagenes();
+
+        // Indicamos que cada 5 segundos cambie la imagen
+        setInterval(rotarImagenes,4000);
+    }
